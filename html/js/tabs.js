95753d92287db6d1d35fa124d14ffe6f040b4640
crazyyy <crazyyy@gmail.com>
2016-06-08 23:36:28 +0300

    js cleaner

--- html/js/tabs.js
+++
@@ -1,2 +0,0 @@
-$.fn.tabs=function(){var t=this;this.each(function(){var e=$(this);$(e.attr("href")).hide(),e.click(function(){return $(t).removeClass("selected"),$(this).addClass("selected"),$($(this).attr("href")).fadeIn(),$(t).not(this).each(function(t,e){$($(e).attr("href")).hide()}),!1})}),$(this).show(),$(this).first().click()};
-//# sourceMappingURL=maps/tabs.js.map
